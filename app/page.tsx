ecc12f7fb5e4f007f81a310167d6e1c3d788e095
kaikySantos <carlos.kaiky62@gmail.com>
2024-11-27 09:52:46 -0300

    feat: wip - landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>Agendou - Landing Page</h1>
+      <Button>
+        <Link href="/auth/sign-in">Entrar</Link>
+      </Button>
+    </div>
+  )
 }
